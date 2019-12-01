07b61857b17494e833f243370752d5c0bb0fdb5a
hirossan <haruto405329@gmail.com>
2019-12-01 21:20:04 +0900

    setting chenge

--- ZisakuZitenRestServer/settings.py
+++ ZisakuZitenRestServer/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database-url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
