1f63b02cf19bff39293cb494dc7301b915c6a1fa
hirossan <haruto405329@gmail.com>
2019-12-02 17:41:20 +0900

    whitenoise changed

--- ZisakuZitenRestServer/settings.py
+++ ZisakuZitenRestServer/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
