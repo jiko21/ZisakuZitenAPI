fec4a7317d6d652906b64591d6b8a461babad900
Daiki Kojima <daikikojima0531@icloud.com>
2019-12-19 17:46:20 +0900

    F: run error in develop mode

--- ZisakuZitenRestServer/settings.py
+++ ZisakuZitenRestServer/settings.py
@@ -15,6 +15,21 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "NAME":"ZisakuZitenAPI",
+        "USER":"Linear",
+        "PASSWORD":"",
+        "POST":"",
+    }
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -23,10 +38,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 ALLOWED_HOSTS = ["127.0.0.1",".herokuapp.com"]
 
@@ -76,27 +87,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ZisakuZitenRestServer.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-import dj_database_url
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        "NAME":"ZisakuZitenAPI",
-        "USER":"Linear",
-        "PASSWORD":"",
-        "POST":"",
-    }
-}
-
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -136,10 +126,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
